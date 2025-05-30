b0ab99309cb44733e060a08837caebd9bedc4e7b
RicardoRoukos <118134506+RicardoRoukos@users.noreply.github.com>
2025-05-31 00:22:17 +0300

    dsf

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -32,7 +32,17 @@ export default function AboutPage() {
           Hello! I’m Hussein Allaw, founder and protector of Al Jourd Ghandi &
           Houssam Allaw Nature Reserve, & your host at Al Jourd Ecotourism &
           Campsite project nestled at 2200 m altitude in the remote highlands
-          intersecting between the regions of Dennieh, Hermel and Akkar...
+          intersecting between the regions of Dennieh, Hermel and Akkar. This
+          sanctuary is a tribute to my only son Ghandi and his cousin Houssam,
+          who tragically passed away few years ago. Today, me, my wife Rima and
+          our two daughters Ghazal and Ghia are commited to preserving the
+          beauty of this land, share its wonder with visitors from all over the
+          world and preserving the memory of Ghandi & Houssam that lives on in
+          the spirit of this reserve. Surrounded by centuries old Juniper trees
+          our land is embeded in the pristine and raw landscape of the unspoilt
+          highands of North Lebanon. It benefits from a unique panorama
+          overlooking Ouadi El Juhannam, the mountains of Dennieh and Akkar to
+          the sea.
         </motion.p>
 
         <motion.div
@@ -49,9 +59,15 @@ export default function AboutPage() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, delay: 0.5 }}
         >
-          Surrounded by centuries old Juniper trees our land is embeded in the
-          pristine and raw landscape of the unspoilt highands of North
-          Lebanon...
+          Our Hidden gem is a safe sanctuary for both the soul and the
+          environment, far from everything, where guests can escape the hustle
+          and bustle of urban life and reconnect with their roots and nature in
+          a meaningful and sustainable way. Through eco-friendly practices, and
+          a deep respect for the local ecosystem, we aspire to be a leading
+          example for regenerative tourism in our region. Join us on this
+          journey as we work towards creating a sanctuary where nature thrives,
+          memories are made, and the legacy of Ghandi and Houssam lives on
+          forever.
         </motion.p>
       </div>
     </section>
